e26ed24b2412bcb88401ceeb3f7fe65e882b6d99
Ana Luisa Dias <analudias@live.com>
2020-07-21 21:42:26 -0300

    Setting changes on button and creating layout

--- app/src/main/java/com/meusprojetos/sorteio/MainActivity.java
+++ app/src/main/java/com/meusprojetos/sorteio/MainActivity.java
@@ -3,6 +3,10 @@ package com.meusprojetos.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+
+        Random random = new Random();
+        int r = random.nextInt(10);
+
+        TextView texto = findViewById(R.id.textoResultado);
+        texto.setText("O número selecionado: " +r);
+
     }
 }
